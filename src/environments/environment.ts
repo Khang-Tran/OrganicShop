262d49569e97f302db5162e498747060b9f8ab61
Khang Tran <piskael03@gmail.com>
2017-11-11 14:06:20 -0500

    Add Firebase configuration

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBSI3RoKEOTw3Z1_Kl3AgG-zkOykfcCB3E",
+    authDomain: "organicshop-4e1ab.firebaseapp.com",
+    databaseURL: "https://organicshop-4e1ab.firebaseio.com",
+    projectId: "organicshop-4e1ab",
+    storageBucket: "",
+    messagingSenderId: "147899117609"
+  }
 };
